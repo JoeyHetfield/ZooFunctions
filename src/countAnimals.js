7efd0bfc1754cadacc4992e7fa527563cf1dc64f
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-29 20:55:21 -0300

    Psrte do Requisito 5, 1/3

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+const animals = data.species;
+// Contagem total de todos animais
+
+const totalAnimals = () => animals.reduce((acc, index) => {
+  acc[index.name] = index.residents.length;
+  return acc;
+}, {});
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return totalAnimals();
+  } return 'testando';
 }
 
+console.log(countAnimals());
+
 module.exports = countAnimals;
+
+/* A função countAnimals é responsável por contar a quantidade de animais que residem no zoológico.
+
+Retorne a quantidade de animais residentes por espécie caso não seja passado nenhum parâmetro. O retorno deverá ser um objeto cujo o nome de cada espécie é a chave e o total de animais (residentes) dessa espécie é o valor. Por exemplo:
+
+  {
+    lions: 4,
+    // [...]
+  }
+Retorne a quantidade de animais residentes no zoológico da espécie passada por parâmetro. Por exemplo:
+
+ao receber o argumento { specie: 'penguins' }, retorna apenas a quantidade (número) de pinguins que residem no zoológico;
+
+ao passar o argumento { specie: 'giraffes', sex: 'female' }, retorna apenas a quantidade (número) de girafas fêmeas que residem no zoológico.
+
+ */
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,6 @@
 const handlerElephants = require('../src/handlerElephants');
-
-describe('Testes da função HandlerElephants', () => {});
+/* describe('Testes da função HandlerElephants', () => {
+  it('Retornar número de elefantes', () => {
+    expect(computeData()).toEqual(4)
+  })
+}) */
