71f5d24e3ad95bb42ff8e419e5017ef48ae48ba6
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-29 16:34:15 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const especies = data.species.find((specie) => specie.name === animal).residents;
+  return especies.every((idade) => idade.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
