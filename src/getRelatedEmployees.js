9172ed0a019562b0a27c78a7a2f2a7d58ce28fd9
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-29 17:09:51 -0300

    Function isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
 function isManager(id) {
-  // seu código aqui
+  if (id === burlId || id === olaId || id === stephanieId) {
+    return true;
+  } return false;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
-
 module.exports = { isManager, getRelatedEmployees };
