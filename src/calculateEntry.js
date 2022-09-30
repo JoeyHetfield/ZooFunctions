36beb6dfadefcce3aae6df8c09bc705ac71cdf89
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-30 18:34:21 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const price = data.prices;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((person) => person.age < 18).length;
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const senior = entrants.filter((person) => person.age >= 50).length;
+  return { child, adult, senior };
 }
 
+// console.log(countEntrants(entrants))
+
 function calculateEntry(entrants) {
-  // seu código aquii
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  } const precoCrianca = countEntrants(entrants).child * price.child;
+  const precoAdulto = countEntrants(entrants).adult * price.adult;
+  const precoIdoso = countEntrants(entrants).senior * price.senior;
+  const total = precoCrianca + precoAdulto + precoIdoso;
+  return total;
 }
+// console.log(calculateEntry(entrants))
 
 module.exports = { calculateEntry, countEntrants };
