e1f712a0b4ff848417de7742230d8f42e0e9a47b
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-30 16:58:18 -0300

    Acredito que completei a 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,6 +1,23 @@
 const handlerElephants = require('../src/handlerElephants');
-/* describe('Testes da função HandlerElephants', () => {
+
+describe('Testes da função HandlerElephants', () => {
   it('Retornar número de elefantes', () => {
-    expect(computeData()).toEqual(4)
+    expect(handlerElephants('count')).toEqual(4)
   })
-}) */
+
+  it('Retornar o nome dos elefantes', () => {
+    expect(handlerElephants('names')).toContain('Jefferson')
+  })
+  
+  it('Retornar média da idade', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5)
+  })
+  
+  it('Retornar se parametro for nulo: ', () => {
+    expect(handlerElephants(undefined)).toBe(undefined)
+  })
+
+  it('Retornar o erro se parametro não for string', () => {
+    expect(handlerElephants(98047)).toBe('Parâmetro inválido, é necessário uma string')
+  })
+}) 
