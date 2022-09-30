a35842d328981057a4daa3e7c7b0695d5deee783
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-30 17:11:04 -0300

    90% dos elefantes

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -2,22 +2,25 @@ const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
   it('Retornar número de elefantes', () => {
-    expect(handlerElephants('count')).toEqual(4)
-  })
+    expect(handlerElephants('count')).toEqual(4);
+  });
 
   it('Retornar o nome dos elefantes', () => {
-    expect(handlerElephants('names')).toContain('Jefferson')
-  })
-  
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+
   it('Retornar média da idade', () => {
-    expect(handlerElephants('averageAge')).toBe(10.5)
-  })
-  
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+
   it('Retornar se parametro for nulo: ', () => {
-    expect(handlerElephants(undefined)).toBe(undefined)
-  })
+    expect(handlerElephants(undefined)).toBe(undefined);
+  });
 
   it('Retornar o erro se parametro não for string', () => {
-    expect(handlerElephants(98047)).toBe('Parâmetro inválido, é necessário uma string')
-  })
-}) 
+    expect(handlerElephants(98047)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Retornar nulo se for um parametro que não é correto', () => {
+    expect(handlerElephants('JoeyHetfield')).toBe(null);
+  });
+});
